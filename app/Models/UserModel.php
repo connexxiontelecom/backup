c747d6943313e2ac11a1f9ab26007c246f4008f4
cjamanambu <oamanambu@yahoo.com>
2021-05-25 13:43:12 +0100

    adds create user function

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,6 @@ class UserModel extends Model {
   protected $table = 'users';
   protected $primaryKey = 'user_id';
   protected $allowedFields = [
-    'login', 'name', 'is_admin', 'email', 'password', 'status'
+    'department_id', 'login', 'name', 'is_admin', 'email', 'password', 'status'
   ];
 }
