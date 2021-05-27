67b61ae17b06818d0eb79e06e15b4e2c35d08057
cjamanambu <oamanambu@yahoo.com>
2021-05-27 10:07:42 +0100

    adds download file function

--- app/Controllers/File.php
+++ app/Controllers/File.php
@@ -14,44 +14,81 @@ class File extends BaseController {
   }
 
   public function upload_file() {
-    $s3 = new S3Client([
-      'region' => getenv('AWS_REGION'),
-      'version' => getenv('VERSION'),
-      'credentials' => [
-        'key' => getenv('ACCESS_KEY_ID'),
-        'secret' => getenv('SECRET_ACCESS_KEY')
-      ],
-    ]);
-    $validated = $this->validate([
-      'file' => ['uploaded[file]'],
-    ]);
-    if ($validated) {
-      $file = $this->request->getFile('file');
+    if ($this->session->active) {
+      $s3 = new S3Client([
+        'region' => getenv('AWS_REGION'),
+        'version' => getenv('VERSION'),
+        'credentials' => [
+          'key' => getenv('ACCESS_KEY_ID'),
+          'secret' => getenv('SECRET_ACCESS_KEY')
+        ],
+      ]);
+      $validated = $this->validate([
+        'file' => ['uploaded[file]'],
+      ]);
+      if ($validated) {
+        $file = $this->request->getFile('file');
+        try {
+          $s3->putObject([
+            'Bucket' => getenv('S3_BUCKET'),
+            'Key' => $file->getClientName(),
+            'SourceFile' => $file->getTempName(),
+          ]);
+          $file_data = [
+            'user_id' => $this->session->get('user_id'),
+            'name' => $file->getClientName(),
+            'mime_type' => $file->guessExtension(),
+            'size' => $file->getSize(),
+            'containing_folder' => 0
+          ];
+          $this->fileModel->save($file_data);
+        } catch (\Exception $e) {
+          $response_data['success'] = false;
+          $response_data['msg'] = $e->getMessage();
+          return $this->response->setJSON($response_data);
+        }
+        $response_data['success'] = true;
+        $response_data['msg'] = 'File upload successful';
+      } else {
+        $response_data['success'] = false;
+        $response_data['msg'] = 'There was a problem uploading your file';
+      }
+      return $this->response->setJSON($response_data);
+    }
+    return redirect('auth');
+  }
+
+  public function download_file($file_id) {
+    if ($this->session->active) {
+      $s3 = new S3Client([
+        'region' => getenv('AWS_REGION'),
+        'version' => getenv('VERSION'),
+        'credentials' => [
+          'key' => getenv('ACCESS_KEY_ID'),
+          'secret' => getenv('SECRET_ACCESS_KEY')
+        ],
+      ]);
+      $file = $this->fileModel->find($file_id);
       try {
-        $s3->putObject([
+        $result = $s3->getObject([
           'Bucket' => getenv('S3_BUCKET'),
-          'Key' => $file->getClientName(),
-          'SourceFile' => $file->getTempName(),
+          'Key' => $file['name'],
         ]);
-        $file_data = [
-          'user_id' => $this->session->get('user_id'),
-          'name' => $file->getClientName(),
-          'mime_type' => $file->guessExtension(),
-          'size' => $file->getSize(),
-          'containing_folder' => 0
-        ];
-        $this->fileModel->save($file_data);
+        header("Content-Type: {$result['ContentType']}");
+        header('Content-Disposition: attachment; filename=' . $file['name']);
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        echo $result['Body'];
       } catch (\Exception $e) {
         $response_data['success'] = false;
         $response_data['msg'] = $e->getMessage();
         return $this->response->setJSON($response_data);
       }
       $response_data['success'] = true;
-      $response_data['msg'] = 'File upload successful';
-    } else {
-      $response_data['success'] = false;
-      $response_data['msg'] = 'There was a problem uploading your file';
+      $response_data['msg'] = 'File download successful';
+      return $this->response->setJSON($response_data);
     }
-    return $this->response->setJSON($response_data);
+    return redirect('auth');
   }
 }
